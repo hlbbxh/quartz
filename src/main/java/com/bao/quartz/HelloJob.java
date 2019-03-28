a4868d58e6a6e2b8fa34a54083de118117b6dc84
yandihu <1394105227@qq.com>
2019-03-28 21:57:42 +0800

    参数的传递和获取

--- src/main/java/com/bao/quartz/HelloJob.java
+++ src/main/java/com/bao/quartz/HelloJob.java
@@ -4,8 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+import org.quartz.TriggerKey;
 
 /**
  * @author Gelil
@@ -14,10 +17,32 @@ import org.quartz.JobExecutionException;
  */
 public class HelloJob implements Job{
 	//实现execute方法
-	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+	public void execute(JobExecutionContext context) throws JobExecutionException {
 		//编写具体的业务逻辑
 		System.out.println("Hello World");
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-HH-dd HH:mm:ss");
 		System.out.println("HelloJob" + sdf.format(new Date()));//输出当时间
+		
+		//打印获取传入参数的值
+		//job的
+		JobKey key = context.getJobDetail().getKey();
+		System.out.println("job:"+key.getGroup()+":"+key.getName());
+		//参数
+		JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+		String msg = jobDataMap.getString("message");
+		float floatjobValue = jobDataMap.getFloat("floatjobValue");
+		System.out.println("job参数:"+ msg +":"+floatjobValue);
+		
+		//trigger的
+		TriggerKey key2 = context.getTrigger().getKey();
+		System.out.println("trigger:"+key2.getGroup()+":"+key2.getName());
+		
+		//参数
+		JobDataMap jobDataMap2 = context.getTrigger().getJobDataMap();
+		String message = jobDataMap2.getString("message");
+		double DoubleTriggerValue = jobDataMap2.getDouble("DoubleTriggerValue");
+		System.out.println("trigger参数:"+ message +":"+DoubleTriggerValue);
+		 
+		
 	}
 }
--- src/main/java/com/bao/quartz/HelloScheduler.java
+++ src/main/java/com/bao/quartz/HelloScheduler.java
@@ -21,16 +21,26 @@ public class HelloScheduler {
 	public static void main(String[] args) {
 		//创建一个jobdetail实例 讲该实例 与  hellojob  class绑定
 		JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
-				.withIdentity("myjob","group1").build();
+				.withIdentity("myjob","group1")
+				.usingJobData("message","hello myjob1")
+				.usingJobData("floatjobValue",3014F)
+				.build();
 		
 		//要点一 
 		System.out.println("jobDetail name"+ jobDetail.getKey().getName());
 		System.out.println("jobDetail group"+ jobDetail.getKey().getGroup());
 		System.out.println("jobDetail jobclass"+ jobDetail.getJobClass().getName());		
 		
+		//要点二
+		//jobDetail  中传入 .usingJobData("message","hello myjob1")
+		//trigger    中传入  .usingJobData("message","hello myTrigger1")
+		//一个key相同  一个不同  类型也不同  在HelloJob JobExecutionContext 获取
+		
 		//创建一个trigger实例  触发器 定义该job立即执行 并且每两秒钟执行一次
 		Trigger trigger = TriggerBuilder.newTrigger()  //工厂模式创建的 new一个
 				.withIdentity("mytrigger","group1") //定义标识符  和组
+				.usingJobData("message","hello myTrigger1")
+				.usingJobData("DoubleTriggerValue",2.0D)
 				.startNow()   //现在执行
 				.withSchedule(SimpleScheduleBuilder
 						.simpleSchedule() //执行频度两秒钟一次
